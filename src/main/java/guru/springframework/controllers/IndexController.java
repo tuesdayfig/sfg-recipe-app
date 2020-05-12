2e5df722dc3d8161d83da68a37f210ecfaa75c6d
Tuesday <dontaskmeaboutit@gmail.com>
2020-05-12 07:51:12 -0400

    lecture 132

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -3,9 +3,7 @@ package guru.springframework.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * Created by jt on 6/1/17.
- */
+
 @Controller
 public class IndexController {
 
