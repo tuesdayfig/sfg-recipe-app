b8aa29c30def285c2b735f8e2f53bdbfd6c7fe35
Tuesday <dontaskmeaboutit@gmail.com>
2020-05-13 04:20:44 -0400

    lecture 165 lombok assignment

--- src/main/java/guru/springframework/bootstrap/RecipeBootStrap.java
+++ src/main/java/guru/springframework/bootstrap/RecipeBootStrap.java
@@ -4,6 +4,7 @@ import guru.springframework.domain.*;
 import guru.springframework.repositories.CategoryRepository;
 import guru.springframework.repositories.RecipeRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class RecipeBootStrap implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -29,6 +31,7 @@ public class RecipeBootStrap implements ApplicationListener<ContextRefreshedEven
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         recipeRepository.saveAll(getRecipes());
+        log.debug("Loading BootStrap Data");
     }
 
     private List<Recipe> getRecipes() {
