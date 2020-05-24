4aa1f4a6c9bfd40c19a435a17c6d1aee7987ff23
Tuesday <dontaskmeaboutit@gmail.com>
2020-05-24 15:46:27 -0400

    lecture 233 data validation

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -63,15 +63,13 @@ public class RecipeControllerTest {
                 .andExpect(view().name("404error"));
     }
 
-//    @Test
-//    public void testGetRecipeNumberFormat() throws Exception {
-//
-//        //when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
-//
-//        mockMvc.perform(get("/recipe/asdf/show"))
-//                .andExpect(status().isBadRequest())
-//                .andExpect(view().name("404error"));
-//    }
+    @Test
+    public void testGetRecipeNumberFormatException() throws Exception {
+
+        mockMvc.perform(get("/recipe/asdf/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
 
     @Test
     public void testGetNewRecipeForm() throws Exception {
@@ -94,9 +92,27 @@ public class RecipeControllerTest {
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .param("id", "")
                 .param("description", "some string")
+                .param("directions", "some directions")
         )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/show"));
+    }
+
+    @Test
+    public void testPostNewRecipeFormValidationFail() throws Exception {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(2L);
+
+        when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+        mockMvc.perform(post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")
+
+        )
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("recipe"))
+                .andExpect(view().name("recipe/recipeform"));
     }
 
     @Test
